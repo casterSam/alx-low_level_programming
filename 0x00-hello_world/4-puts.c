4049f772214533e25215f6165cf67cccaf3ca0ba
casterSam <castermashp@gmail.com>
2022-12-10 14:46:15 -0800

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
-/**
- *
- *   * main - The entry point of C programs
- *
- *     * @void: The function takes no parameters
- *
- *       *
- *
- *         * Description: The function "puts" writes to stdout.
- *
- *           * Return: 0
+/*
+ * main - The entry point of C programs
+ * @void: The function takes no parameters
+ * Description: The function "puts" writes to stdout.
+ * Return: 0
  */
 int main(void)
 {
