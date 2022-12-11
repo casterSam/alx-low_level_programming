85bd6d46001e88473ac8a6aa40dc3f3c28aabfa6
casterSam <castermashp@gmail.com>
2022-12-10 17:03:12 -0800

    c programs updates

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: The function main takes no arguments
+ *
+ * Return:Always 0.
+ */
+int main(void)
+{
+	char u;
+	int v;
+	long int w;
+	long long int x;
+	float y;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(u));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(v));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(w));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(y));
+	return (0);
+}
