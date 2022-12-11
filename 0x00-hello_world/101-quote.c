dfcadde4b9f646ac4b81d9f8542239cfde07f629
casterSam <castermashp@gmail.com>
2022-12-11 01:06:05 -0800

    low level

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - start of program c
+ * Description: The function "main" takes no parameters
+ * Return:Always 0 (sucess)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
