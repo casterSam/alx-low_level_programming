40fa2f4c1f74a568daf736722502472840bf5d50
casterSam <castermashp@gmail.com>
2022-12-10 16:14:32 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                                                                        
+
+/**                                                                                                                                                                       
+ * main - The entry point of C programs
+ *
+ * Description: The function "main" takes no parameters                                                                                                              
+ *
+ *  Return:Always 0 (sucess)                                                                                                                                         
+ */
+int main(void)                                                                                                                                                            
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);   
+
+}          
