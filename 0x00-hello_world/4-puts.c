81f4705451c6c5afde0ddd5008797014cd2415ed
casterSam <castermashp@gmail.com>
2022-12-10 16:38:11 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
  * main - The entry point of C programs
- *
  * Description: The function "main" takes no parameters
- *
  * Return:Always 0 (sucess)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - The entry point of C programs
  * Description: The function "main" takes no parameters
- * Return:Always 0 (sucess)
+ * Return:Always 0 (sucess).
  */
-int main(void)                                                                                                                                                            
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}          
+}
