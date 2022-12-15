985020dfb39269b5665aa86ff8fc65f0691ac6b2
casterSam <castermashp@gmail.com>
2022-12-15 14:13:46 -0800

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - The entry point of the function
+ *  @a: First argument
+ *  @b: Second argument
+ *  Description: The function adds two integers
+ *  Return: sum
+ */
+int add (int a, int b)
+{
+	int sum= a + b;
+	return (sum);
+}
