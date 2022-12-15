c9a9ab627203fe7fbc480b8de7f8e8b544595ac7
casterSam <castermashp@gmail.com>
2022-12-15 12:31:26 -0800

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - The entry point of the program
+ * @x: The argument to be evaluated
+ * Description: _abs function that computes the absolute value of an
+ * Return: abs_value
+ */
+int _abs(int x)
+{
+	int abs_value;
+
+	if (n < 0)
+		abs_value = -1 * n;
+	else
+		abs_value = n;
+	return (abs_value);
+}
