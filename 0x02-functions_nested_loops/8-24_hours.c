92949611b5dab028d2b5e8cf40308aea78873dc2
casterSam <castermashp@gmail.com>
2022-12-15 12:59:34 -0800

    update

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer -unction entry
+ * @void: The function takes no parameters
+ * Description:jack_bauer function that prints every minute of the day 
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	h = 0;
+
+	while (h < 24)
+	{
+		m = 0;
+
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar(m / 10) + '0');
+			_putchar(m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
