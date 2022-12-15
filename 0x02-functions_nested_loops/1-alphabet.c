1394fd9433967d447122a7b1d047433a37d0839f
casterSam <castermashp@gmail.com>
2022-12-15 08:43:15 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,11 @@
  */
 void print_alphabet(void)
 {
-	char lowercase_alphabet='a';
+	char lowercase_alphabet = 'a';
 		while (lowercase_alphabet <= 'z')
 		{
 			_putchar(lowercase_alphabet);
 			lowercase_alphabet++;
 		}
 	_putchar('\n');
-		
 }
