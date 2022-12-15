04667a31e9a9719f871eedc43c84423ff7627ca9
casterSam <castermashp@gmail.com>
2022-12-15 08:40:17 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  * Description:  This function prints lowercase alphabet
  * Return: Always 0.
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char lowercase_alphabet='a';
 		while (lowercase_alphabet <= 'z')
