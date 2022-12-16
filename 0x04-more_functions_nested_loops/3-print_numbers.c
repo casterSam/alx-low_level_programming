335491d206be64f9fb002daed5ce581422a5b4e7
casterSam <castermashp@gmail.com>
2022-12-16 14:57:59 -0800

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - tHIS FUNCTIONS PRINTS 0-9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int s = '0';
+
+	for (; S <= '9'; i++)
+	{
+		_putchar(s);
+	}
+	_putchar('\n');
+}
